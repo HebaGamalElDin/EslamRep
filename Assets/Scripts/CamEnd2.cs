e6d0ae2b6bd48bee8efa6ccd08059c574dc6d87c
EslamOsamaAhmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2018-04-21 04:16:29 +0200

    Added Caslte Scene
    Added Caslte Scene

---
+++ Assets/Scripts/CamEnd2.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamEnd2 : MonoBehaviour {
+
+	public void OnAnimationEnd() {
+		AnimationCam2.instance.DestroyCamera(gameObject);
+	}
+}
+
