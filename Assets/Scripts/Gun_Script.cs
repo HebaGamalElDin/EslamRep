a757f13a57290978dba18f121b54f7ef9da403f1
EslamOsamaAhmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2018-04-27 20:32:34 +0200

    Semi-Final-Version
    Semi-Final-Version

---
+++ Assets/Scripts/Gun_Script.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+using UnityEngine.Assertions;
+public class Gun_Script : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 20f;
+    public Camera fps;
+    [SerializeField] private Transform bulletGune;
+    [SerializeField] private GameObject bulletprefap;
+    //public ParticleSystem muzzleFlash;
+
+	// Use this for initialization
+	void Start () {
+        Assert.IsNotNull(bulletprefap);
+        Assert.IsNotNull(bulletGune);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           
+            shoot();
+            fire_bullet();
+        }
+
+	}
+    void shoot()
+    {
+       // muzzleFlash.Play();
+        RaycastHit hitinfo;
+        
+        if (Physics.Raycast(fps.transform.position,fps.transform.forward,out hitinfo,range))
+        {
+             
+            if (hitinfo.distance <= range)
+                hitinfo.transform.SendMessage("take_Damage", damage, SendMessageOptions.DontRequireReceiver);
+            Debug.Log(hitinfo.transform.name);
+            Target_Script target = hitinfo.transform.GetComponent<Target_Script>();
+            //if (target != null)
+           // {
+            //    target.take_Damage(damage);
+            //}
+
+        }
+    }
+    void fire_bullet()
+    {
+        GameObject fireball = Instantiate(bulletprefap,bulletGune.position,bulletGune.rotation) as GameObject;
+        // fireball.GetComponent<Rigidbody>().velocity = fireball.transform.forward * 4;
+        fireball.GetComponent<Rigidbody>().AddForce(fireball.transform.forward * 1000);
+        Destroy(fireball,3f);
+    }
+    
+    
+}
---
+++ Assets/Scripts/Target_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Target_Script : MonoBehaviour {
+
+    public float health=50f;
+
+    public void take_Damage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        Destroy(gameObject);
+    }
+	
+}
